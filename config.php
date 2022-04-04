6952ebd8d8e84ef3dbf84c4214e19583058ea1b7
Jasper <89900143+97088656@users.noreply.github.com>
2022-04-04 11:21:15 +0200

    test

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "tado";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+if (!$conn) {
+    die("<script>alert('Connection Failed.')</script>");
+}
+;)
+?>
+
